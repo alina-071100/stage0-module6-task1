4d3163371e87131d0c1d7f96a178a5b354ab985e
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-07-27 17:44:12 +0400

    Class creation

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String fur = !hasFur ? " no fur." : " a fur.";
+        String paws = numberOfPaws > 1 ? " paws " : " paw ";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + paws + "and" + fur;
+    }
 }
